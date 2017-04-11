7d32dd003ec6afde6fb18a8bfc8ee4adcde8a20a
Bill Sacks <sacks@ucar.edu>
2017-04-11 13:51:13 -0600

    Document new whichflwa option, and fix latex for existing option

--- libglide/glide_types.F90
+++ libglide/glide_types.F90
@@ -349,11 +349,11 @@ module glide_types
     integer :: whichflwa = 2
 
     !> \begin{description} 
-    !> \item[0] Set equal to $1\times 10^{-16}\,\mathrm{yr}^{-1}
+    !> \item[0] Set equal to $1\times 10^{-16} \mathrm{Pa}^{-n} \mathrm{yr}^{-1}$
     !> \item[1] \emph{Paterson and Budd} relationship, 
     !> with temperature set to $-5^{\circ}\mathrm{C}$ 
-    !> \item[2] \emph{Paterson and Budd} relationship 
-    !> \,\mathrm{Pa}^{-n}$
+    !> \item[2] \emph{Paterson and Budd} relationship
+    !> \item[3] Read flwa/flwastag from file
     !> \end{description}
 
     integer :: whichbtrc = 0
