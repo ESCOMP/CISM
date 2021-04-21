44f771c90c2df1df1c19632d7d05d86b20abc726
William Lipscomb <lipscomb@ucar.edu>
2021-04-21 10:37:52 -0600

    Do not call init_isostasy unless isostasy = 1
    The code was calling subroutine init_isostasy with isostasy = 0 = ISOSTASY_NONE.
    This subroutine is now called only if isostasy = 1 = ISOSTASY_COMPUTE.

--- libglissade/glissade.F90
+++ libglissade/glissade.F90
@@ -457,7 +457,11 @@ contains
     ! handle relaxed/equilibrium topo
     ! Initialise isostasy first
 
-    call init_isostasy(model)
+    if (model%options%isostasy == ISOSTASY_COMPUTE) then
+
+       call init_isostasy(model)
+
+    endif
 
     select case(model%isostasy%whichrelaxed)
 
