5c099ce3ae1054f9264ed4f7052b08e91e44aaeb
Tim van den Akker <timvandenakker96@gmail.com>
2024-04-25 10:44:58 -0600

    With build files included

---
+++ builds/derecho-intel/CMakeFiles/3.26.3/CMakeFortranCompiler.cmake
@@ -0,0 +1,61 @@
+set(CMAKE_Fortran_COMPILER "/glade/u/apps/derecho/23.06/spack/opt/spack/ncarcompilers/1.0.0/oneapi/2023.0.0/ec7b/bin/mpi/mpif90")
+set(CMAKE_Fortran_COMPILER_ARG1 "")
+set(CMAKE_Fortran_COMPILER_ID "Intel")
+set(CMAKE_Fortran_COMPILER_VERSION "2021.8.0.20221119")
+set(CMAKE_Fortran_COMPILER_WRAPPER "")
+set(CMAKE_Fortran_PLATFORM_ID "Linux")
+set(CMAKE_Fortran_SIMULATE_ID "")
+set(CMAKE_Fortran_COMPILER_FRONTEND_VARIANT "")
+set(CMAKE_Fortran_SIMULATE_VERSION "")
+
+
+
+
+set(CMAKE_AR "/usr/bin/ar")
+set(CMAKE_Fortran_COMPILER_AR "")
+set(CMAKE_RANLIB "/usr/bin/ranlib")
+set(CMAKE_Fortran_COMPILER_RANLIB "")
+set(CMAKE_COMPILER_IS_GNUG77 )
+set(CMAKE_Fortran_COMPILER_LOADED 1)
+set(CMAKE_Fortran_COMPILER_WORKS TRUE)
+set(CMAKE_Fortran_ABI_COMPILED TRUE)
+
+set(CMAKE_Fortran_COMPILER_ENV_VAR "FC")
+
+set(CMAKE_Fortran_COMPILER_SUPPORTS_F90 1)
+
+set(CMAKE_Fortran_COMPILER_ID_RUN 1)
+set(CMAKE_Fortran_SOURCE_FILE_EXTENSIONS f;F;fpp;FPP;f77;F77;f90;F90;for;For;FOR;f95;F95)
+set(CMAKE_Fortran_IGNORE_EXTENSIONS h;H;o;O;obj;OBJ;def;DEF;rc;RC)
+set(CMAKE_Fortran_LINKER_PREFERENCE 20)
+if(UNIX)
+  set(CMAKE_Fortran_OUTPUT_EXTENSION .o)
+else()
+  set(CMAKE_Fortran_OUTPUT_EXTENSION .obj)
+endif()
+
+# Save compiler ABI information.
+set(CMAKE_Fortran_SIZEOF_DATA_PTR "8")
+set(CMAKE_Fortran_COMPILER_ABI "ELF")
+set(CMAKE_Fortran_LIBRARY_ARCHITECTURE "")
+
+if(CMAKE_Fortran_SIZEOF_DATA_PTR AND NOT CMAKE_SIZEOF_VOID_P)
+  set(CMAKE_SIZEOF_VOID_P "${CMAKE_Fortran_SIZEOF_DATA_PTR}")
+endif()
+
+if(CMAKE_Fortran_COMPILER_ABI)
+  set(CMAKE_INTERNAL_PLATFORM_ABI "${CMAKE_Fortran_COMPILER_ABI}")
+endif()
+
+if(CMAKE_Fortran_LIBRARY_ARCHITECTURE)
+  set(CMAKE_LIBRARY_ARCHITECTURE "")
+endif()
+
+
+
+
+
+set(CMAKE_Fortran_IMPLICIT_INCLUDE_DIRECTORIES "/glade/u/apps/derecho/23.06/spack/opt/spack/netcdf/4.9.2/oneapi/2023.0.0/iijr/include;/glade/u/apps/derecho/23.06/spack/opt/spack/hdf5/1.12.2/oneapi/2023.0.0/d6xa/include;/glade/u/apps/derecho/23.06/opt/include;/glade/u/apps/derecho/23.06/spack/opt/spack/intel-oneapi-mpi/2021.8.0/oneapi/2023.0.0/mhf4/mpi/2021.8.0/include/gfortran;/glade/u/apps/derecho/23.06/spack/opt/spack/intel-oneapi-mpi/2021.8.0/oneapi/2023.0.0/mhf4/mpi/2021.8.0/include;/glade/u/apps/derecho/23.06/spack/opt/spack/intel-oneapi-mkl/2023.0.0/oneapi/2023.0.0/f2sh/mkl/2023.0.0/include;/glade/u/apps/common/23.04/spack/opt/spack/intel-oneapi-compilers/2023.0.0/compiler/2023.0.0/linux/compiler/include/intel64;/glade/u/apps/common/23.04/spack/opt/spack/intel-oneapi-compilers/2023.0.0/compiler/2023.0.0/linux/compiler/include/icc;/glade/u/apps/common/23.04/spack/opt/spack/intel-oneapi-compilers/2023.0.0/compiler/2023.0.0/linux/compiler/include;/usr/local/include;/usr/lib64/gcc/x86_64-suse-linux/7/include;/usr/lib64/gcc/x86_64-suse-linux/7/include-fixed;/usr/include")
+set(CMAKE_Fortran_IMPLICIT_LINK_LIBRARIES "mpifort;mpi;rt;pthread;dl;ifport;ifcoremt;imf;svml;m;ipgo;irc;pthread;svml;dl;c;gcc;gcc_s;irc_s;dl;c")
+set(CMAKE_Fortran_IMPLICIT_LINK_DIRECTORIES "/glade/u/apps/derecho/23.06/spack/opt/spack/netcdf/4.9.2/oneapi/2023.0.0/iijr/lib;/glade/u/apps/derecho/23.06/spack/opt/spack/intel-oneapi-mkl/2023.0.0/oneapi/2023.0.0/f2sh/mkl/2023.0.0/lib/intel64;/glade/u/apps/common/23.04/spack/opt/spack/intel-oneapi-compilers/2023.0.0/lib;/glade/u/apps/derecho/23.06/spack/opt/spack/hdf5/1.12.2/oneapi/2023.0.0/d6xa/lib;/glade/u/apps/derecho/23.06/opt/lib64;/glade/u/apps/derecho/23.06/opt/lib;/glade/u/apps/derecho/23.06/spack/opt/spack/intel-oneapi-mpi/2021.8.0/oneapi/2023.0.0/mhf4/mpi/2021.8.0/lib/release;/glade/u/apps/derecho/23.06/spack/opt/spack/intel-oneapi-mpi/2021.8.0/oneapi/2023.0.0/mhf4/mpi/2021.8.0/lib;/glade/u/apps/common/23.04/spack/opt/spack/intel-oneapi-compilers/2023.0.0/compiler/2023.0.0/linux/compiler/lib/intel64_lin;/glade/u/apps/common/23.04/spack/opt/spack/intel-oneapi-compilers/2023.0.0/compiler/2023.0.0/linux/lib;/usr/lib64/gcc/x86_64-suse-linux/7;/usr/lib64;/lib64;/usr/x86_64-suse-linux/lib;/lib;/usr/lib")
+set(CMAKE_Fortran_IMPLICIT_LINK_FRAMEWORK_DIRECTORIES "")
---
+++ builds/derecho-intel/cism_driver/CMakeFiles/cism_driver.dir/DependInfo.cmake
@@ -0,0 +1,48 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "Fortran"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_Fortran
+  "/glade/u/home/tvda/CISM/cism_driver/cism_driver.F90" "/glade/u/home/tvda/CISM/builds/derecho-intel/cism_driver/CMakeFiles/cism_driver.dir/cism_driver.F90.o"
+  "/glade/u/home/tvda/CISM/cism_driver/cism_external_dycore_interface.F90" "/glade/u/home/tvda/CISM/builds/derecho-intel/cism_driver/CMakeFiles/cism_driver.dir/cism_external_dycore_interface.F90.o"
+  "/glade/u/home/tvda/CISM/cism_driver/cism_front_end.F90" "/glade/u/home/tvda/CISM/builds/derecho-intel/cism_driver/CMakeFiles/cism_driver.dir/cism_front_end.F90.o"
+  "/glade/u/home/tvda/CISM/cism_driver/gcm_cism_interface.F90" "/glade/u/home/tvda/CISM/builds/derecho-intel/cism_driver/CMakeFiles/cism_driver.dir/gcm_cism_interface.F90.o"
+  "/glade/u/home/tvda/CISM/cism_driver/gcm_to_cism_glint.F90" "/glade/u/home/tvda/CISM/builds/derecho-intel/cism_driver/CMakeFiles/cism_driver.dir/gcm_to_cism_glint.F90.o"
+  )
+set(CMAKE_Fortran_COMPILER_ID "Intel")
+set(CMAKE_Fortran_SUBMODULE_SEP "@")
+set(CMAKE_Fortran_SUBMODULE_EXT ".smod")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_Fortran
+  "CISM_MPI"
+  "GLIMMER_MPI"
+  )
+
+# The include file search paths:
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH
+  "fortran_mod_files"
+  "fortran_autocopy_includes"
+  "/glade/u/apps/derecho/23.06/spack/opt/spack/netcdf/4.9.2/oneapi/2023.0.0/iijr/include"
+  "."
+  "/glade/u/home/tvda/CISM"
+  "/glade/u/home/tvda/CISM/libglide"
+  "include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  "/glade/u/home/tvda/CISM/builds/derecho-intel/CMakeFiles/glimmercismfortran.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "/glade/u/home/tvda/CISM/builds/derecho-intel/fortran_mod_files")
