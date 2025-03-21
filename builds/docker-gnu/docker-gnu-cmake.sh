8cf532080247d3811b7343f87448a8d2dfdc0241
katetc <katec@ucar.edu>
2025-03-21 02:01:16 +0000

    Docker build directory

---
+++ builds/docker-gnu/docker-gnu-cmake.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+
+# After this script completes, type: make -j 8
+# If rebuilding, type 'make clean' before running 'make -j 8'
+
+# Set path to top cism directory
+# Note, this is an easy way to build out of source.
+# In directory you want to build in, run:
+#   $ source $CISM/builds/linux-gnu-cism/linux-gnu-cism-cmake $CISM
+# where $CISM is the path to the top level cism directory.
+if [ $# -eq 0 ]
+then
+    cism_top="../.." 
+else
+    cism_top=${1%/}
+fi
+
+#source /etc/profile.d/z00_modules.sh
+
+#source derecho-intel-modules
+
+echo CISM: "${cism_top}"
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH/usr/lib64:
+export LIBRARY_PATH=$LIBRARY_PATH/usr/lib64:
+
+# remove old build data:
+rm -f ./CMakeCache.txt
+rm -rf ./CMakeFiles
+
+echo
+echo "Doing CMake Configuration step"
+
+# Note: the compilation flags were taken from the defaults for a CESM build on
+# cheyenne-intel (using cime at 84aafd5). Some of these options are probably
+# unnecessary for a standalone cism build, but I am keeping things consistent
+# with the CESM build for simplicity.
+
+# CISM_USE_GPTL_INSTRUMENTATION -- ON by default, set to OFF to not use GPTL instrumentation.
+
+cmake \
+  -D CISM_BUILD_CISM_DRIVER:BOOL=ON \
+  -D CISM_ENABLE_BISICLES=OFF \
+  -D CISM_ENABLE_FELIX=OFF \
+\
+  -D CISM_USE_TRILINOS:BOOL=OFF \
+  -D CISM_MPI_MODE:BOOL=ON \
+  -D CISM_SERIAL_MODE:BOOL=OFF \
+\
+  -D CISM_USE_GPTL_INSTRUMENTATION:BOOL="${CISM_USE_GPTL_INSTRUMENTATION:=ON}" \
+  -D CISM_COUPLED:BOOL=OFF \
+  -D CISM_USE_CISM_FRONT_END:BOOL=OFF \
+\
+  -D CISM_NETCDF_DIR="$NETCDF" \
+  -D CISM_NETCDF_LIBS="netcdff" \
+\
+  -D CISM_GPTL_DIR= "utils/libgptl" \
+  -D CMAKE_VERBOSE_MAKEFILE:BOOL=OFF \
+\
+  -D CMAKE_CXX_COMPILER=mpicxx \
+  -D CMAKE_C_COMPILER=mpicc \
+  -D CMAKE_Fortran_COMPILER=mpifort \
+\
+  -D CISM_EXTRA_LIBS:STRING="-L/usr/lib64 -llapack" \
+  -D CMAKE_EXE_LINKER_FLAGS="-Wl,--copy-dt-needed-entries" \
+\
+  -D CMAKE_Fortran_FLAGS:STRING="-g -O2 -ffree-line-length-none -fallow-argument-mismatch" \
+  -D CMAKE_C_FLAGS:STRING="-g -O2" \
+  -D CMAKE_CXX_FLAGS:STRING="-g -O2" \
+  "${cism_top}"
+
